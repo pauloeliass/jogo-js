9f3fbb03436681aff237ff1a0b293f3eec50b828
pauloeliass <paulohenrique13@hotmail.com.br>
2022-09-15 21:43:55 -0300

    Interface improved, game over screen, score system

--- js/script.js
+++ js/script.js
@@ -1,40 +1,48 @@
 let scoreCount = "0";
 let scoreBlock = false;
 const jumpTime = 500;
+let marioIsAlive = true;
 
 const jump = () => {
   mario.classList.add("jump");
 
   setTimeout(() => {
-    mario.classList.remove("jump");
+    if (marioIsAlive) mario.classList.remove("jump");
   }, jumpTime);
 };
 
 function marioIsJumpingUpPipe(pipePosition, marioPosition) {
-    return pipePosition <= 120 && pipePosition > 0 && marioPosition >= 80;
-  }  
-
-function marioIsDead(pipePosition, marioPosition) {
-  return pipePosition <= 120 && pipePosition > 0 && marioPosition < 80;
+  return pipePosition <= 120 && pipePosition > 0 && marioPosition >= 80;
 }
 
-const loop = setInterval(() => {
+function marioIsDead(pipePosition, marioPosition, pipeTopPosition) {
+  return (
+    pipePosition <= 120 && pipePosition > 0 && marioPosition <= pipeTopPosition
+  );
+}
+
+function stopGame(pipePosition, marioPosition) {
+  pipe.style.animationPlayState = "paused";
+
+  mario.style.animationPlayState = "paused";
+
+  mario.src = "./images/game-over.png";
+  mario.style.width = "75px";
+  mario.style.marginLeft = "50px";
+  clouds.style.animationPlayState = "paused"; // pausa a animação das núvens. //
+  gameOver.style.display = 'flex'; // isso serve pra alterar a propriedade CSS //
+}
+
+function loop() {
   const pipePosition = pipe.offsetLeft;
+  const pipeTopPosition = pipe.offsetHeight;
   const marioPosition = +window
     .getComputedStyle(mario)
     .bottom.replace("px", "");
 
-  if (marioIsDead(pipePosition, marioPosition)) {
-    pipe.style.animation = "none";
-    pipe.style.left = `${pipePosition}px`;
-
-    mario.style.animation = "none";
-    mario.style.bottom = `${marioPosition}px`;
-
-    mario.src = "./images/game-over.png";
-    mario.style.width = "75px";
-    mario.style.marginLeft = "50px";
-
+  if (marioIsDead(pipePosition, marioPosition, pipeTopPosition)) {
+    marioIsAlive = false;
+    stopGame(pipePosition, marioPosition);
     clearInterval(loop);
   } else if (marioIsJumpingUpPipe(pipePosition, marioPosition)) {
     if (!scoreBlock) {
@@ -45,6 +53,8 @@ const loop = setInterval(() => {
       }, jumpTime);
     }
   }
-}, 10);
-
+  requestAnimationFrame(loop);
+}
+loop();
 document.addEventListener("keydown", jump);
+restartBtn.addEventListener('click', ()=> location.reload())
